b9f042b406ca43e6f7405d4a682796101ef7be48
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2023-12-19 08:47:13 +0100

    Add files via upload

---
+++ godrok/Godorcs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace godrok
+{
+    internal class Godorcs
+    {
+            public int Eleje { get; set; }
+            public int Vege { get; set; }
+            public List<int> Adatok { get; set; }
+            public Godorcs()
+            {
+                Adatok = new List<int>();
+            }
+            override public string ToString()
+            {
+                string sor = Adatok[0].ToString();
+                for (int i = 1; i < Adatok.Count; i++)
+                {
+                    sor += " " + Adatok[i].ToString();
+                }
+                return sor;
+            }
+            public bool Melyulo()
+            {
+                int e = 1;
+                while (e < Adatok.Count && Adatok[e - 1] <= Adatok[e]) e++;
+                int v = Adatok.Count - 2;
+                while (v >= 0 && Adatok[v + 1] <= Adatok[v]) v--;
+                return e > v;
+            }
+            public int Maxmely()
+            {
+                int max = Adatok[0];
+                for (int i = 1; i < Adatok.Count; i++)
+                    if (max < Adatok[i])
+                        max = Adatok[i];
+                return max;
+            }
+
+            public int V(int szint)
+            {
+                int szum = 0;
+                for (int i = 0; i < Adatok.Count; i++)
+                {
+                    if (Adatok[i] - szint > 0)
+                        szum += Adatok[i] - szint;
+                }
+                return szum * 10;
+            }
+        }
+    }
+
---
+++ godrok/Program.cs
@@ -0,0 +1,92 @@
+﻿﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace godrok
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ut = new int[2001];
+            int meter = 0;
+            List<Godorcs> godrok = new List<Godorcs>();
+            Console.WriteLine("1. feladat");
+            StreamReader olvaso = new StreamReader("melyseg.txt");
+            while (!olvaso.EndOfStream)
+            {
+                meter++;
+                ut[meter] = int.Parse(s: olvaso.ReadLine());
+            }
+            Console.WriteLine("A fájl adatainak száma: {0}", meter);
+            olvaso.Close();
+            Console.WriteLine("2. feladat");
+            Console.Write("Adjon meg egy távolságértéket! ");
+            int hely = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ezen a helyen a felszín {0} méter mélyen van.", ut[hely]);
+            Console.WriteLine("3. feladat");
+            int sima = 0;
+            for (int i = 0; i < meter; i++)
+                if (ut[i] == 0)
+                    sima++;
+            Console.WriteLine("Az érintetlen terület aránya {0:P2}.", (double)sima / meter);
+            #region 4. feladat - Gödrök készítése, kiírása
+            int m = 2;
+            while (m < meter)
+            {
+                if (ut[m - 1] == 0 && ut[m] != 0) 
+                {
+                    Godorcs ujgodor = new Godorcs();
+                    ujgodor.Eleje = m;
+                    ujgodor.Adatok.Add(ut[m]);
+                    m++;
+                    while (ut[m] != 0)
+                    {
+                        ujgodor.Adatok.Add(ut[m]);
+                        m++;
+                    }
+                    ujgodor.Vege = m - 1;
+                    godrok.Add(ujgodor);
+                }
+                m++;
+            }
+            StreamWriter kiiro = new StreamWriter("godrok.txt");
+            for (int i = 0; i < godrok.Count; i++)
+            {
+                kiiro.WriteLine(godrok[i]);
+            }
+            kiiro.Close();
+            #endregion
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("A gödrök száma: {0}", godrok.Count);
+
+            Console.WriteLine("6. feladat");
+            int ez = 0;
+            while (godrok[ez].Vege < hely)
+                ez++;
+            bool G = godrok[ez].Eleje <= hely && godrok[ez].Vege >= hely;
+            if (!G)
+            {
+                Console.WriteLine("Az adott helyen nincs gödör.");
+            }
+            else
+            {
+                Console.WriteLine("a)");
+                Console.WriteLine("A gödör kezdete: {0} méter, a gödör vége: {1} méter.", godrok[ez].Eleje, godrok[ez].Vege);
+
+                Console.WriteLine("b)");
+                Console.WriteLine(godrok[ez].Melyulo() ? "Folyamatosan mélyül." : "Nem mélyül folyamatosan.");
+
+                Console.WriteLine("c)");
+                Console.WriteLine("A legnagyobb mélysége {0} méter.", godrok[ez].Maxmely());
+
+                Console.WriteLine("d)");
+                Console.WriteLine("A térfogata {0} m ^ 3.", godrok[ez].V(0));
+
+                Console.WriteLine("e)");
+                Console.WriteLine("A vízmennyiség {0} m ^ 3.", godrok[ez].V(1));
+            }
+        }
+    }
+}
